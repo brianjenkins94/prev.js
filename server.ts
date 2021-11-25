5136df5d70329cdf8f953ace1d949d84111f7540
Brian Jenkins <brianjenkins94@gmail.com>
2021-11-24 21:58:37 -0500

    No top-level `await` :(

--- server.ts
+++ server.ts
@@ -6,27 +6,27 @@ const dev = process.env["NODE_ENV"] !== "production";
 
 const app = next({
 	"conf": {
-		"useFileSystemPublicRoutes": false
+		//"useFileSystemPublicRoutes": false
 	},
 	"dev": dev
 });
 
 const handle = app.getRequestHandler();
 
-await app.prepare();
+app.prepare().then(function() {
+	createServer(function(request, response) {
+		// @ts-expect-error
+		const parsedUrl = parse(request.url, true);
+		const { pathname, query } = parsedUrl;
 
-createServer(function(request, response) {
-	// @ts-expect-error
-	const parsedUrl = parse(request.url, true);
-	const { pathname, query } = parsedUrl;
-
-	if (pathname === "/a") {
-		app.render(request, response, "/a", query);
-	} else if (pathname === "/b") {
-		app.render(request, response, "/b", query);
-	} else {
-		handle(request, response, parsedUrl);
-	}
-}).listen(3000, function() {
-	console.log("> Ready on http://localhost:3000");
+		if (pathname === "/a") {
+			app.render(request, response, "/a", query);
+		} else if (pathname === "/b") {
+			app.render(request, response, "/b", query);
+		} else {
+			handle(request, response, parsedUrl);
+		}
+	}).listen(3000, function() {
+		console.log("> Ready on http://localhost:3000");
+	});
 });
