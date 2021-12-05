b4d6812e24b352131f09d5363fc4290c865fa10a
Brian Jenkins <brianjenkins94@gmail.com>
2021-12-05 18:50:09 -0500

    Hardcoded path

--- bootstrap.js
+++ bootstrap.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 let directory = process.cwd()
 
 const options = {
-	"cwd": directory,
+	"cwd": "/Users/bjenks/.npm/_npx/59469f58d9fd3e53",
 	"encoding": "utf8",
 	"stdio": "inherit"
 };
