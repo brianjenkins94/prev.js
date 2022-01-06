8997a724226e74991cfe4e8d3c6dafea087cb005
Brian Jenkins <brianjenkins94@gmail.com>
2022-01-06 18:03:54 -0500

    Fully remove `Eta`

--- renderer.ts
+++ renderer.ts
@@ -1,83 +1,83 @@
 function parse(template) {
-    const buffer = [];
+	const buffer = [];
 
-    const openTagRegex = /<%\s*(=|-)?\s*/gu;
-    const closeTagRegex = /'|"|`|\/\*|(\s*%>)/gu;
+	const openTagRegex = /<%\s*(=|-)?\s*/gu;
+	const closeTagRegex = /'|"|`|\/\*|(\s*%>)/gu;
 
-    let index = 0;
-    let match;
+	let index = 0;
+	let match;
 
-    while (match = openTagRegex.exec(template)) {
-        index = match[0].length + match.index;
-        const prefix = match[1] ?? "";
+	while (match = openTagRegex.exec(template)) {
+		index = match[0].length + match.index;
+		const prefix = match[1] ?? "";
 
-        closeTagRegex.lastIndex = index;
+		closeTagRegex.lastIndex = index;
 
-        let closeTag;
+		let closeTag;
 
-        while (closeTag = closeTagRegex.exec(template)) {
-            if (closeTag[1] !== undefined) {
-                const value = template.slice(index, closeTag.index);
+		while (closeTag = closeTagRegex.exec(template)) {
+			if (closeTag[1] !== undefined) {
+				const value = template.slice(index, closeTag.index);
 
-                index = closeTagRegex.lastIndex;
-                openTagRegex.lastIndex = index;
+				index = closeTagRegex.lastIndex;
+				openTagRegex.lastIndex = index;
 
-                buffer.push({
-                    "type": {
-                        "": "execute",
-                        "-": "raw",
-                        "=": "interpolate"
-                    }[prefix],
-                    "value": value
-                });
+				buffer.push({
+					"type": {
+						"": "execute",
+						"-": "raw",
+						"=": "interpolate"
+					}[prefix],
+					"value": value
+				});
 
-                break;
-            }
-        }
-    }
+				break;
+			}
+		}
+	}
 
-    return buffer;
+	return buffer;
 }
 
 function compile(nodes) {
-    const buffer = ["const buffer = [];"];
+	const buffer = ["const buffer = [];"];
 
-    for (const node of nodes) {
-        if (typeof node === "string") {
-            buffer.push("buffer.push(\"" + node + "\");");
-        } else {
-            let { type, value } = node;
+	for (const node of nodes) {
+		if (typeof node === "string") {
+			buffer.push("buffer.push(\"" + node + "\");");
+		} else {
+			let { type, value } = node;
 
-            switch (type) {
-                case "interpolate":
-                    if (/[&<>"']/gu.test(value)) {
-                        value = value.replace(/[&<>"']/gu, function(character) {
-                            return {
-                                "&": "&amp;",
-                                "<": "&lt;",
-                                ">": "&gt;",
-                                "\"": "&quot;",
-                                "'": "&#39;"
-                            }[character];
-                        });
-                    }
-                case "raw":
-                    buffer.push("buffer.push(" + value + ");");
-                    break;
-                case "execute":
-                    buffer.push(value);
-                    break;
-                default:
-            }
-        }
-    }
+			switch (type) {
+				case "interpolate":
+					if (/[&<>"']/gu.test(value)) {
+						value = value.replace(/[&<>"']/gu, function(character) {
+							return {
+								"&": "&amp;",
+								"<": "&lt;",
+								">": "&gt;",
+								"\"": "&quot;",
+								"'": "&#39;"
+							}[character];
+						});
+					}
+				case "raw":
+					buffer.push("buffer.push(" + value + ");");
+					break;
+				case "execute":
+					buffer.push(value);
+					break;
+				default:
+			}
+		}
+	}
 
-    buffer.push("return buffer.join(\"\\n\");");
+	buffer.push("return buffer.join(\"\\n\");");
 
-    return buffer.join("\n");
+	return buffer.join("\n");
 }
 
 export function render(template, data) {
-    // eslint-disable-next-line no-new-func, @typescript-eslint/no-implied-eval
-    return new Function(...Object.keys(data), compile(parse(template)))(...Object.values(data));
+	// eslint-disable-next-line no-new-func, @typescript-eslint/no-implied-eval
+	return new Function(...Object.keys(data), compile(parse(template)))(...Object.values(data));
 }
