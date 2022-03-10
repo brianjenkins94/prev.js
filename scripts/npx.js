1ea9c0af0381259e54b8fc8c11d51f50efb42486
Brian Jenkins <brianjenkins94@gmail.com>
2022-03-10 15:37:22 -0500

    Maybe

--- scripts/npx.js
+++ scripts/npx.js
@@ -1,51 +1,12 @@
 #!/usr/bin/env node
 
-import { spawnSync } from "child_process";
-import * as fs from "fs";
 import * as path from "path";
 import * as url from "url";
+import { main as spawn } from "ts-node/dist/bin"
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let baseDirectory = path.join(__dirname, "..");
 
-const options = {
-	"cwd": process.cwd(),
-	"encoding": "utf8",
-	"env": {
-		...process.env,
-		"TS_NODE_PROJECT": path.join(baseDirectory, "tsconfig.json"), // https://github.com/TypeStrong/ts-node/pull/1655
-		"TS_NODE_SKIP_IGNORE": true
-	},
-	"stdio": "inherit"
-};
-
-if (process.platform === "win32") {
-	const bash = path.join(process.env.ProgramW6432, "Git", "usr", "bin", "bash.exe");
-	const wsl = path.join(process.env.windir, "System32", "bash.exe");
-
-	if (fs.existsSync(bash)) {
-		options["shell"] = url.pathToFileURL(bash).toString();
-	} else if (fs.existsSync(wsl)) {
-		const { root, dir, base } = path.parse(directory);
-
-		directory = "/mnt/" + root.split(":")[0].toLowerCase() + "/" + dir.substring(root.length).replace(/\\/g, "/") + "/" + base;
-
-		options["shell"] = url.pathToFileURL(wsl).toString();
-	} else {
-		throw new Error("No suitable shell found. Aborting.");
-	}
-}
-
-const command = [
-	"node",
-	"--experimental-specifier-resolution=node",
-	"--loader=" + url.pathToFileURL(path.join(baseDirectory, "..", "ts-node", "esm.mjs")).toString(),
-	url.pathToFileURL(path.join(baseDirectory, "scripts", "prev.ts")).toString(),
-	...process.argv.slice(2)
-];
-
-console.log("\n> " + command.join(" ") + "\n");
-
-spawnSync(command[0], command.slice(1), options);
+spawn(["--esm", path.join(baseDirectory, "prev.ts"), ...process.argv.slice(2)]);
