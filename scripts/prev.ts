ff457ec0d916827688afd13745aafaee88aab87c
Brian Jenkins <brianjenkins94@gmail.com>
2022-03-28 15:24:00 -0400

    idk

--- scripts/prev.ts
+++ scripts/prev.ts
@@ -240,7 +240,7 @@ if (argv["recursive"] === true && argv["update"] === true) {
 	if (!fs.existsSync(path.join(baseDirectory, "package.json"))) {
 		console.log(process.argv);
 		console.log(argv);
-		execSync("npm init" + (argv["yes"] === true) ? " --yes" : "", { "cwd": baseDirectory, "stdio": "inherit" });
+		execSync("npm init" + (argv["yes"] === true ? " --yes" : ""), { "cwd": baseDirectory, "stdio": "inherit" });
 		console.log();
 
 		fs.writeFileSync(path.join(baseDirectory, "package.json"), fs.readFileSync(path.join(baseDirectory, "package.json"), { "encoding": "utf-8" }).replace("\"main\": \"index.js\"", "\"type\": \"module\""));
